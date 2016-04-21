071b3fe12f5f0f2e18bc5b18c77d7dc7421987ee
markytop <marcos_osilva@live.com>
2016-04-21 12:22:25 -0300

    Criando pacotes

---
+++ SupoteInfotec/src/main/java/br/com/supote/supoteinfotec/util/HibernateUtil.java
@@ -0,0 +1,39 @@
+
+package br.com.supote.supoteinfotec.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+/**
+ *
+ * @author Marcos
+ */
+
+
+public class HibernateUtil {
+  private static final SessionFactory sessionFactory;
+  public static final String HIBERNATE_SESSION = "hibernate_session";
+  
+  static{
+      
+      try{
+          Configuration configuration = new Configuration().configure();
+          
+          ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().
+                applySettings(configuration.getProperties()).buildServiceRegistry();
+          
+          
+       sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            
+            System.out.println("Session factory criada corretamente");
+        } catch (Exception ex) {
+            
+            System.out.println("Ocorreu um  erro ao iniciar a SF. " + ex);
+            throw new ExceptionInInitializerError(ex);
+      }
+  }
+    
+    
+}
